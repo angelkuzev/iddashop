538f4fb157daae45d908ab8f8162dfa91fdb0fd8
angelkuzev <103533507+angelkuzev@users.noreply.github.com>
2022-04-13 00:59:10 +0300

    Revert "Try to ignore mediafiles"
    This reverts commit 16015f8230a2d1fcac65d3c69b3d401501a80085.

--- iddashop/main/models.py
+++ iddashop/main/models.py
@@ -1,3 +1,25 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    NAME_MAX_LENGTH = 30
+    NAME_MIN_LENGTH = 2
+    DESCRIPTION_MAX_LENGTH = 200
+    DESCRIPTION_MIN_LENGTH = 2
+
+    picture = models.ImageField()
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(NAME_MIN_LENGTH),
+        )
+    )
+
+    description = models.TextField(
+        max_length=DESCRIPTION_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(DESCRIPTION_MIN_LENGTH),
+        )
+    )
