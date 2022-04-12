a3577af546ef2e91ab250c3e8419a353d7f5d15f
angelkuzev <103533507+angelkuzev@users.noreply.github.com>
2022-04-13 00:46:40 +0300

    Added simple item model and functionality
    TODO: Extend them

--- iddashop/main/urls.py
+++ iddashop/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from iddashop.main.views import HomePageView
+from iddashop.main import views as views
 
 urlpatterns = (
-    path('', HomePageView.as_view(), name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('add-item/', views.CreateItemView.as_view(), name='create item'),
 )
--- iddashop/main/views.py
+++ iddashop/main/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views import generic as views
+from iddashop.main.models import Item
 
 
-class HomePageView(TemplateView):
+class HomePageView(views.ListView):
+    model = Item
     template_name = 'home.html'
+    context_object_name = 'items'
+
+
+class CreateItemView(views.CreateView):
+    model = Item
+    template_name = 'add_product.html'
+    fields = ('picture', 'name', 'description')
+    success_url = reverse_lazy('home')
+
+
